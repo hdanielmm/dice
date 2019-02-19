8225aa955f39ec31e2210a6434b757d38f51d4a8
Daniel <hdanielmm@users.noreply.github.com>
2019-02-19 23:33:13 +0100

    Pagina web dados
    Refrescar pagina para tirar dados.

---
+++ dice.js
@@ -0,0 +1,20 @@
+var player1 = Math.floor(Math.random() * 6) + 1;
+var player2 = Math.floor(Math.random() * 6) + 1;
+
+while (player1 === player2) {
+  var player1 = Math.floor(Math.random() * 6) + 1;
+  var player2 = Math.floor(Math.random() * 6) + 1;
+}
+
+var randomImageSource1 = "img/dice" + player1 + ".png";
+var randomImageSource2 = "img/dice" + player2 + ".png";
+
+
+if (player1 > player2) {
+  document.querySelector("h1").textContent = "Player 1 Wins!";
+} else {
+  document.querySelector("h1").textContent = "Player 2 Wins!";
+}
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
