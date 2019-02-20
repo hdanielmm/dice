316cc6e9ae6b2ad40a0a0c00b770e68dc5ee96bc
Daniel <hdanielmm@users.noreply.github.com>
2019-02-20 20:06:11 +0100

    Update dice.js

--- dice.js
+++ dice.js
@@ -1,20 +1,24 @@
-var player1 = Math.floor(Math.random() * 12) + 1;
-var player2 = Math.floor(Math.random() * 12) + 1;
+document.querySelector("a").addEventListener("click", jugar);
 
-while (player1 === player2) {
+function jugar() {
   var player1 = Math.floor(Math.random() * 12) + 1;
   var player2 = Math.floor(Math.random() * 12) + 1;
+
+  while (player1 === player2) {
+    var player1 = Math.floor(Math.random() * 12) + 1;
+    var player2 = Math.floor(Math.random() * 12) + 1;
+  }
+
+  var randomImageSource1 = "img/dice" + player1 + ".png";
+  var randomImageSource2 = "img/dice" + player2 + ".png";
+
+
+  if (player1 > player2) {
+    document.querySelector("h1").textContent = "Player 1 Wins!";
+  } else {
+    document.querySelector("h1").textContent = "Player 2 Wins!";
+  }
+
+  document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
+  document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 }
-
-var randomImageSource1 = "img/dice" + player1 + ".png";
-var randomImageSource2 = "img/dice" + player2 + ".png";
-
-
-if (player1 > player2) {
-  document.querySelector("h1").textContent = "Player 1 Wins!";
-} else {
-  document.querySelector("h1").textContent = "Player 2 Wins!";
-}
-
-document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
